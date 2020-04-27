f864e6e27650a6731934cfe2a937b642905c958d
Lysk <kroyer.github@gmail.com>
2020-04-26 22:57:19 -0700

    init ship

--- cotc_env/envs/ship.py
+++ cotc_env/envs/ship.py
@@ -6,9 +6,10 @@ from cotc_env.envs.constants import *
 
 class Ship:
 
-    def __init__(self):
-        self.cap = randint(0, 5)
-        self.center = Cell(randint(0, X_MAX - 1), randint(0, Y_MAX - 1))
+    def __init__(self, cell=None, cap=None):
+        if cell is None and cap is None:
+            self.cap = randint(0, 5)
+            self.center = Cell(randint(0, X_MAX - 1), randint(0, Y_MAX - 1))
         self.bow = None
         self.prow = None
         self._set_bow_prow()
