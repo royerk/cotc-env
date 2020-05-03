0555ebcb81ae0059f7673be69937cf1d3dab618a
Lysk <kroyer.github@gmail.com>
2020-05-03 16:36:41 -0700

    Add utils test

---
+++ cotc_env/tests/test_utils.py
@@ -0,0 +1,7 @@
+from cotc_env.envs.utils import *
+
+def test_get_opposite_cap():
+    assert get_opposite_cap(0) == 3
+    assert get_opposite_cap(2) == 5
+    assert get_opposite_cap(5) == 2
+    assert get_opposite_cap(3) == 0
