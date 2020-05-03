6ea1e1e32f12dc50747623d16a077fa58bc0ea6b
Lysk <kroyer.github@gmail.com>
2020-05-03 13:21:18 -0700

    Add cell turn

--- cotc_env/envs/cell.py
+++ cotc_env/envs/cell.py
@@ -1,3 +1,4 @@
+from cotc_env.envs.constants import *
 from cotc_env.envs.constants import oddr_directions
 
 
@@ -11,7 +12,12 @@ class Cell:
         return Cell(self.q + dq, self.r + dr)
 
     def get_port_cell(self, cap):
-        pass
+        dq, dr = oddr_directions[self.r % 2][(cap + 2) % 6]
+        return Cell(self.q + dq, self.r + dr)
 
-    def get_stat_cell(self, cap):
-        pass
+    def get_star_cell(self, cap):
+        dq, dr = oddr_directions[self.r % 2][(cap - 2) % 6]
+        return Cell(self.q + dq, self.r + dr)
+
+    def is_in_map(self):
+        return 0 <= self.q < MAP_WIDTH and 0 <= self.r < MAP_HEIGHT
