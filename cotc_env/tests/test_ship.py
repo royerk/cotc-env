eb7fc5d21b92ecaec6105688e28fdbec3ab0bda7
Lysk <kroyer.github@gmail.com>
2020-06-13 12:28:45 -0700

    Update test ship with new coords

--- cotc_env/tests/test_ship.py
+++ cotc_env/tests/test_ship.py
@@ -13,27 +13,19 @@ def test_create():
 
 def test_create_cap():
     ship = Ship(cell=Cell(0, 0), cap=2)
-    assert ship.prow == Cell(-1, -1)
+    assert ship.prow == Cell(0, -1)
     assert ship.center == Cell(0, 0)
     assert ship.stern == Cell(0, 1)
     assert ship.cap == 2
 
 
-def test_forward_even():
-    ship = Ship(cell=Cell(0, 0), cap=0)
+def test_forward():
+    ship = Ship(cell=Cell(0, 0), cap=4)
     ship.move_forward()
-    assert ship.prow == Cell(2, 0), "prow " + ship.prow.to_string()
-    assert ship.center == Cell(1, 0), "center" + ship.center.to_string()
+    assert ship.prow == Cell(-2, 2), "prow " + ship.prow.to_string()
+    assert ship.center == Cell(-1, 1), "center" + ship.center.to_string()
     assert ship.stern == Cell(0, 0), "stern " + ship.stern.to_string()
-    assert ship.cap == 0
-
-
-def test_forward_odd():
-    ship = Ship(cell=Cell(0, 1), cap=5)
-    ship.move_forward()
-    assert ship.prow == Cell(1, 3), "prow " + ship.prow.to_string()
-    assert ship.center == Cell(1, 2), "center" + ship.center.to_string()
-    assert ship.stern == Cell(0, 1), "stern " + ship.stern.to_string()
+    assert ship.cap == 4
 
 
 def test_turn_port():
@@ -46,9 +38,9 @@ def test_turn_port():
 
 
 def test_turn_star():
-    ship = Ship(cell=Cell(0, 1), cap=3)
+    ship = Ship(cell=Cell(0, 0), cap=3)
     ship.turn(STAR)
-    assert ship.prow == Cell(0, 0), "prow " + ship.prow.to_string()
-    assert ship.center == Cell(0, 1), "center" + ship.center.to_string()
-    assert ship.stern == Cell(1, 2), "stern " + ship.stern.to_string()
+    assert ship.prow == Cell(0, -1), "prow " + ship.prow.to_string()
+    assert ship.center == Cell(0, 0), "center" + ship.center.to_string()
+    assert ship.stern == Cell(0, 1), "stern " + ship.stern.to_string()
     assert ship.cap == 2
