2619a8c7fc6dfcff57665116f0cebcee1c1d2f19
kroyer <kroyer@unity3d.com>
2020-06-15 13:44:59 -0700

    Add mirror function to utils

--- cotc_env/envs/utils.py
+++ cotc_env/envs/utils.py
@@ -7,8 +7,9 @@ def get_opposite_cap(cap):
     return (cap + 3) % 6
 
 
-def get_random_axial():
-    r = randint(1, MAP_HEIGHT - 2)
+def get_random_axial(r=None):
+    if r is None:
+        r = randint(1, MAP_HEIGHT - 2)
     q = randint(-r // 2 + 1, MAP_WIDTH - r // 2 - 2)
     return q, r
 
@@ -21,3 +22,9 @@ def get_random_offset():
 
 def get_2d_from_axial(q, r):
     return q + r // 2, r
+
+
+def get_mirror_axial(q, r):
+    mirror_r = MAP_HEIGHT - 1 - r
+    mirror_q = (q + r // 2) - mirror_r // 2
+    return mirror_q, mirror_r
