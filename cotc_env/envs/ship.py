f946f2f8977f3bcde6207a60e4afb2168439ff3c
kroyer <kroyer@unity3d.com>
2020-06-15 09:01:02 -0700

    Use utils to generate position

--- cotc_env/envs/ship.py
+++ cotc_env/envs/ship.py
@@ -2,7 +2,7 @@ from random import randint
 
 from cotc_env.envs.cell import Cell
 from cotc_env.envs.constants import *
-from cotc_env.envs.utils import get_opposite_cap
+from cotc_env.envs.utils import get_opposite_cap, get_random_axial
 
 
 class Ship:
@@ -13,8 +13,7 @@ class Ship:
             raise ValueError("Ship init need neither or both cell and cap.")
         elif cell is None and cap is None:
             self.cap = randint(0, 5)
-            r = randint(1, MAP_HEIGHT - 2)
-            q = randint(-r//2 + 1, MAP_WIDTH - r // 2 - 2)
+            q, r = get_random_axial()
             self.center = Cell(q, r) 
         else:
             self.center = cell
