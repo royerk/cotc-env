3777046d3c3ec37c367350e9bc7075597eed76ba
kroyer <kroyer@unity3d.com>
2020-06-15 13:49:20 -0700

    Remove print

--- cotc_env/envs/state_solo.py
+++ cotc_env/envs/state_solo.py
@@ -65,7 +65,6 @@ class StateSolo:
 
     def _set_map_value(self, q, r, channel):
         x, y = get_2d_from_axial(q, r)
-        print(q, r, "->", x, y)
         self.map[x][y][channel] = 1
 
     def _clear_map_value(self, q, r, channel):
